a25a7be738316b1c8eb4d74a058434e0b5469d1c
turkan kaya <tukrankaya.mdbf14@iste.edu.tr>
2020-07-09 14:34:34 +0300

    efCore added.

---
+++ TodoApplication/Data/TodoDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApplication.Models;
+
+namespace TodoApplication.Data
+{
+    public class TodoDbContext : DbContext
+    {
+
+        public TodoDbContext(DbContextOptionsBuilder builder) : base(builder.Options)
+        {
+        }
+        public DbSet<TodoItem> TodoItems
+        {
+            get; set;
+        }
+            // Metot ile ayarların yapılması
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        
+          
+            }
+        }
+        
---
+++ TodoApplication/Models/builder.cs
@@ -0,0 +1,6 @@
+﻿namespace TodoApplication.Models
+{
+    internal class builder
+    {
+    }
+}
